dc5472e627ab8251ad6ba13c66935a99e6271998
Dhaval Solanki <dhavalas24@gmail.com>
2024-12-05 09:45:01 +0530

    5/12

---
+++ constructor/unary.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+class complex{
+    int x;
+    int y;
+    public:
+    void getdata(int a, int b){
+        x=a;
+        y=b;
+    }
+    void printdata(){
+        cout<<"value of x:"<<x<<endl;
+        cout<<"value of y:"<<y<<endl;
+    }
+    void operator-(){
+        x=-x;
+        y=-y;
+    }
+};
+int main(){
+    class complex s1;
+    s1.getdata(-5,3);
+    s1. printdata();
+
+    cout<<"after function call:"<<endl;
+    -s1;
+    s1.printdata();
+
+}
